f5198a953c89297ae111b3d250c335b190fbd618
Susumu Yamazaki <zacky1972@gmail.com>
2018-05-31 06:51:36 +0900

    change to int64

--- logistic_map.c
+++ logistic_map.c
@@ -2,36 +2,42 @@
 #include <stdlib.h>
 #include <math.h>
 #include <sys/time.h>
+#include <time.h>
+#include <stdint.h>
 
 #define LOOP 10
 #define P 6700417
 #define MU 22
 #define NUM_VALUES 0x2000000
 
-static int logisticsmap_calc(int x, int p, int mu) {
+static int64_t logisticsmap_calc(int64_t x, int64_t p, int64_t mu) {
     return mu * x * (x + 1) % p;
 }
 
-static int logisticsmap_loopCalc(int num, int x, int p, int mu) {
-    for(int i = 0; i < num; i++) {
+static int64_t logisticsmap_loopCalc(int64_t num, int64_t x, int64_t p, int64_t mu) {
+    for(int64_t i = 0; i < num; i++) {
         x = logisticsmap_calc(x, p, mu);
     }
     return x;
 }
 
 void benchmark() {
-	const int mu = MU;
-	const int p = P;
-	int *x;
-	x = (int *)malloc(sizeof(int) * NUM_VALUES);
+    const int64_t mu = MU;
+    const int64_t p = P;
+    int64_t *x;
 
-	for(int i = 1; i < NUM_VALUES; i++) {
-		x[i] = i;
-	}
-	for(int i = 1; i < NUM_VALUES; i++) {
-		x[i] = logisticsmap_loopCalc(LOOP, x[i], p, mu);
-	}
-	free(x);	
+    x = (int64_t *)malloc(sizeof(int64_t) * NUM_VALUES);
+
+    for(int i = 1; i < NUM_VALUES; i++) {
+        x[i] = i;
+    }
+    for(int i = 1; i < NUM_VALUES; i++) {
+        x[i] = logisticsmap_loopCalc(LOOP, x[i], p, mu);
+    }
+
+    printf("1: %lld, 10000: %lld\n", x[1], x[10000]);
+
+    free(x);
 }
 
 int main() {
